834ef7fb1e7784cb15712a6707851e90de55bc3c
Henry Rocha <henryrocha@protonmail.com>
2020-10-27 21:09:57 -0300

    Add providers

---
+++ providers-vars.tf
@@ -0,0 +1,7 @@
+variable "region_01" {
+  default = "us-east-1"
+}
+
+variable "region_02" {
+  default = "us-east-2"
+}
---
+++ providers.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  alias      = "region_01"
+  region     = var.region_01
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+provider "aws" {
+  alias      = "region_02"
+  region     = var.region_02
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
