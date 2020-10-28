4d03c20dd8f9cfc9468544843c146aa6f1cb1671
Henry Rocha <henryrocha@protonmail.com>
2020-10-27 21:11:24 -0300

    add docker install script

---
+++ install_docker.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
