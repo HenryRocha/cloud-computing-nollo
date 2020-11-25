e43504e3cb841d1fc3268c5a77584282acd4ee50
Henry Rocha <henryrocha@protonmail.com>
2020-11-25 01:07:54 -0300

    Add nat to backend wireguard

--- infrastructure/startup-scripts/setup-backend-wireguard.sh
+++ infrastructure/startup-scripts/setup-backend-wireguard.sh
@@ -16,8 +16,8 @@ cat > ./wg0.conf <<EOF
 [Interface]
 Address = 10.10.150.20/32
 SaveConfig = true
-PostUp = iptables -A FORWARD -i wg0 -j ACCEPT
-PostDown = iptables -D FORWARD -i wg0 -j ACCEPT
+PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
 ListenPort = 51820
 PrivateKey = ${WG_BE_SERVER_PVK}
 
