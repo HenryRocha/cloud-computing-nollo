943200597b5d3f02456de48c215d1c582cf6427b
Henry Rocha <henryrocha@protonmail.com>
2020-11-26 09:36:45 -0300

    Add reverse proxy on frontend, to fix access to backend

--- infrastructure/startup-scripts/setup-nollo-site.sh
+++ infrastructure/startup-scripts/setup-nollo-site.sh
@@ -14,7 +14,22 @@ cd /home/ubuntu
 
 # Clone the repo
 git clone https://github.com/HenryRocha/cloud-computing-nollo.git
-cd cloud-computing-nollo/nollo-site/
 
-sudo docker build -t nollo-site --build-arg API_URL="http://${NOLLO_API_LB_DNS}" .
-sudo docker run -d -p 80:5000 --name=run-nollo-site nollo-site
+# Replace the placeholder on Caddyfile.
+sudo sed -i 's/\NOLLO_API_LB_DNS/${NOLLO_API_LB_DNS}/g' /home/ubuntu/cloud-computing-nollo/reverse-proxy/Caddyfile
+
+# Build the Svelte app container.
+sudo docker build -t nollo-site \
+--build-arg API_URL="http://${NOLLO_SITE_LB_DNS}" \
+/home/ubuntu/cloud-computing-nollo/nollo-site/
+
+# Run the Svelte app container.
+sudo docker run -d -p 8080:5000 --name=run-nollo-site nollo-site
+
+# Create and run the reverse proxy container.
+sudo docker run -d -p 80:80 \
+-v /home/ubuntu/cloud-computing-nollo/reverse-proxy/Caddyfile:/etc/caddy/Caddyfile \
+-v /home/ubuntu/cloud-computing-nollo/reverse-proxy/caddy_data:/data \
+--name=nollo-caddy \
+--network=host \
+caddy
